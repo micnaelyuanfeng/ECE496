9b14794a740b5f54387c422c6649f4f52444678d
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2017-03-15 13:46:35 -0700

    improve readability

--- Adafruit HAT controllers/dc_motor_test.c
+++ Adafruit HAT controllers/dc_motor_test.c
@@ -5,19 +5,28 @@
 #include "Adafruit_MotorHAT.h"
 #include "Adafruit_ServoHAT.h"
 
+#define MOTORADDRESS 0x60
+#define CHANNELTEST 0
+#define SPEEDTEST 220
+#define DIRECTIONTEST 1
+#define FREQUENCY 1600
+
 int main(){
+	// Generic setup
 	map_peripheral_BCM2835(&gpio);
 	map_peripheral_BCM2835(&bsc0);
 	init_I2C_protocol();
-	init_PCA9685(0x60);
-	set_PWM_frequency_PCA9685(0x60, 1600);
-	stop_DC_motor(0x60, 0);
-	stop_DC_motor(0x60, 1);
-	stop_DC_motor(0x60, 2);
-	stop_DC_motor(0x60, 3);
-
-	set_speed_DC_motor(0x60, 0, 220);
-	set_direction_DC_motor(0x60, 0, 1);
+	// Board setup
+	init_PCA9685(MOTORADDRESS);
+	set_PWM_frequency_PCA9685(MOTORADDRESS, FREQUENCY);
+	stop_DC_motor(MOTORADDRESS, 0);
+	stop_DC_motor(MOTORADDRESS, 1);
+	stop_DC_motor(MOTORADDRESS, 2);
+	stop_DC_motor(MOTORADDRESS, 3);
+	
+	// DC motor control 
+	set_speed_DC_motor(MOTORADDRESS, CHANNELTEST, SPEEDTEST);
+	set_direction_DC_motor(MOTORADDRESS, CHANNELTEST, DIRECTIONTEST);
 
 	return 0;
 }
