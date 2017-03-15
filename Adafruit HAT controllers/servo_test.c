14ae25bf025a1fbcf2a9fe3fa2586ab8ff834207
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2017-03-15 13:50:48 -0700

    improve readability

--- Adafruit HAT controllers/servo_test.c
+++ Adafruit HAT controllers/servo_test.c
@@ -9,18 +9,24 @@
 #define SERVOHATADDR 0x40
 #define CHANNELTEST 0
 int main(){
+	// Generic setup
 	map_peripheral_BCM2835(&gpio);
 	map_peripheral_BCM2835(&bsc0);
 	init_I2C_protocol();
+	
+	// Board setup
 	init_PCA9685(SERVOHATADDR);
 	set_PWM_frequency_PCA9685(SERVOHATADDR, FREQUENCY);
+	init_angle_to_pulse_length_lookup_table();
+	
+	// debug begin
 	int middle = pulse_length_to_tick_converter(FREQUENCY, 1500);
 	int min = pulse_length_to_tick_converter(FREQUENCY, 500);
 	int max = pulse_length_to_tick_converter(FREQUENCY, 2500);
 	printf("min: %d, middle: %d, max: %d\n", min, middle, max);
-	init_angle_to_pulse_length_lookup_table();
+	// debug end
 	while(1){
-		//set_PWM_PCA9685(SERVOHATADDR, CHANNELTEST, 0, min);	
+		// servo control by degree 
 		set_servo(SERVOHATADDR, CHANNELTEST, FREQUENCY, 0);
 		sleep(5);
 		set_servo(SERVOHATADDR, CHANNELTEST, FREQUENCY, 90);
