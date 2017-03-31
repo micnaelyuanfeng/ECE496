8be74d22a97085d6eb2316cc6dcfcec94320a1e2
hehesky <lk.genesis@163.com>
2017-03-31 15:40:37 -0400

    Web Browser
    执行文件在bin\x86\Debug

---
+++ CefTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CefTest
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser browser;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.google.com");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+            this.AutoScroll = true;
+            this.FormClosing += onFormClosing;
+            
+        }
+        private void onFormClosing(object s,EventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
